f8f688b07b7bfc479ce29561e0c176f817a84cef
Nate Page <npage14@gmail.com>
2019-11-28 22:22:30 -0700

    Added client code and changed esp32 to access point

---
+++ Server/DaVinciBotClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.IO.File;
+using System.Net.Sockets;
+
+/// <summary>
+/// Client class for the DaVinci Bot.  Loads commands from gcode file and passes them one by one to the esp32 access point.
+/// </summary>
+public class DaVinciBotClient
+{
+	public static void Main()
+    {
+        TcpClient client = new TcpClient();
+        Console.WriteLine("Connecting...");
+
+        client.Connect("192.168.4.1", 80);
+
+        Console.WriteLine("Connected");
+
+        string[] commands = System.IO.File.ReadAllLines(@".\commands.ncc");
+
+        int numCommands = commands.Length;
+
+        Stream stream = client.GetStream();
+
+        int count = 0;
+        int index = 0;
+
+        foreach(string line in lines)
+        {
+            byte[] command = line;
+
+            if (count == 0)
+            {
+                byte[] request = new byte[5];
+
+                while (request == null) { request = stream.Read(request, 0, 5); }
+
+                count = BitConverter.ToInt32(request, 0);
+            }
+
+            stream.Write(command, 0, command.Length);
+
+            count--;
+            index++;
+
+            Console.WriteLine(index / numCommands);
+        }
+    }
+}
